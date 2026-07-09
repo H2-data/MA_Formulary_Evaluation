66b2cc2105ad22f8bccc49179999283f4f0cbb61
h2-data <haydenleebarns@gmail.com>
2026-07-09 14:44:17 +0800

    SQL Adjusment and pythin sanity check.

--- med_liability_score.sql
+++ med_liability_score.sql
@@ -2,9 +2,9 @@
 -- First, I'll create the raw scores for claims, beneficiaries, and spending.
 -- I'll also create inverted and uninverted options for claims and beneficiaries.
 
-DROP VIEW raw_scores;
+DROP VIEW v_raw_scores;
 
-CREATE VIEW raw_scores AS
+CREATE VIEW v_raw_scores AS
 SELECT
     l.Brnd_Name,
     l.HCPCS_Cd,
@@ -17,19 +17,15 @@ SELECT
     PERCENT_RANK() OVER(PARTITION BY l.year ORDER BY b.Tot_Benes) AS tot_benes_true
 FROM lookup AS l
 JOIN meds AS m
-    ON l.Brnd_Name = m.Brnd_Name
-    AND l.year = m.year
-    AND l.HCPCS_Cd = m.HCPCS_Cd
+    ON l.HCPCS_Cd = m.HCPCS_Cd
 JOIN benes AS b
-    ON l.Brnd_Name = b.Brnd_Name
-    AND l.year = b.year
-    AND l.HCPCS_Cd = b.HCPCS_Cd;
+    ON l.HCPCS_Cd = b.HCPCS_Cd;
 
-SELECT * FROM raw_scores;
+SELECT * FROM v_raw_scores;
 
-DROP VIEW raw_growth_scores;
+DROP VIEW v_raw_growth_scores;
 
-CREATE VIEW raw_growth_scores AS
+CREATE VIEW v_raw_growth_scores AS
 SELECT
     g1.Brnd_Name,
     PERCENT_RANK() OVER(ORDER BY g1.pct_change) AS growth_21_22_rank,
@@ -40,14 +36,14 @@ JOIN v_growth_22_23 AS g2
 
 SELECT
     *
-FROM raw_growth_scores
+FROM v_raw_growth_scores
 ORDER BY growth_22_23_rank DESC;
 
 
 -- I have all the raw scores, but the final scores will be calculated with weight for each aspect. 
-DROP VIEW final_scores;
+DROP VIEW v_final_scores;
 
-CREATE VIEW final_scores AS
+CREATE VIEW v_final_scores AS
 SELECT
     r.Brnd_Name,
     r.year,
@@ -64,20 +60,27 @@ SELECT
       + 0.10 * g.growth_22_23_rank
       + 0.05 * g.growth_21_22_rank
     ) AS composite_score
-FROM raw_scores AS r
-JOIN raw_growth_scores AS g
+FROM v_raw_scores AS r
+JOIN v_raw_growth_scores AS g
     ON r.Brnd_Name = g.Brnd_Name;
 
 SELECT
     *
-FROM final_scores
+FROM v_final_scores
 ORDER BY composite_score DESC;
+
+SELECT
+    Brnd_Name,
+    COUNT(Brnd_Name)
+FROM v_final_scores
+GROUP BY Brnd_Name
+HAVING COUNT(Brnd_Name) > 5;
 
 -- I plan to use the weighted scores, but here, I'll create a view for the unweighted scores just to have on hand.
 
-DROP VIEW final_scores_unw;
+DROP VIEW v_final_scores_unw;
 
-CREATE VIEW final_scores_unw AS
+CREATE VIEW v_final_scores_unw AS
 SELECT
     r.Brnd_Name,
     r.year,
@@ -95,20 +98,27 @@ SELECT
         + g.growth_22_23_rank
         + g.growth_21_22_rank
     ) / 5 AS composite_score_unw
-FROM raw_scores AS r
-JOIN raw_growth_scores AS g
+FROM v_raw_scores AS r
+JOIN v_raw_growth_scores AS g
     ON r.Brnd_Name = g.Brnd_Name;
 
 SELECT
     *
-FROM final_scores_unw
+FROM v_final_scores_unw
 ORDER BY composite_score_unw DESC;
+
+SELECT
+    Brnd_Name,
+    COUNT(Brnd_Name)
+FROM v_final_scores_unw
+GROUP BY Brnd_Name
+HAVING COUNT(Brnd_Name) > 5;
 
 SELECT 
 	Brnd_Name,
     year,
     ROUND(composite_score, 2) as Composite_Score
-FROM final_scores
+FROM v_final_scores
 WHERE year = 2023
 ORDER BY composite_score DESC;
 
